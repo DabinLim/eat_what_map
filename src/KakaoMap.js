f9bfad232e76c75f6177f81f3400450507688df8
dabinim <dabinim09@slogup.com>
2021-09-20 00:09:13 +0900

    initialize app

---
+++ src/KakaoMap.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import styled from "styled-components";
+import {useEffect} from "react";
+
+const KaKaoMap = () => {
+
+
+    useEffect(() => {
+    const { kakao } = window
+        let container = document.getElementById('map');
+        let options = {
+            center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+            level: 3,
+        }
+        let map = new kakao.maps.Map(container, options)
+    },[])
+
+    return(
+        <Container id="map">
+
+        </Container>
+    )
+}
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+export default KaKaoMap;
